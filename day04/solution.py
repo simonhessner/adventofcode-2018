6320774a0d1d0fbacfb4540b56d996c8b6dd2784
Simon Hessner <uldci@student.kit.edu>
2018-12-04 03:29:12 -0500

    Improve code for day 04

--- day04/solution.py
+++ day04/solution.py
@@ -1,20 +1,18 @@
 # https://adventofcode.com/2018/day/4
 
 from collections import defaultdict
-from collections import Counter
+import operator
 
 with open('input') as f:
 	lines = f.read().splitlines()
 
-	guards = defaultdict(lambda : Counter({ minute : 0 for minute in range(60) }))
+	# date format is nice (big time unit -> small time unit: year-month-day hour:minute) => normal sort is enough
+	lines.sort()
+
+	sleep_per_minute = defaultdict(lambda : defaultdict(int))
+
 	guard_id = -1
 	sleepstart = -1
-
-	# I just realized that you do not have to do the date conversion since the format is already nice (big time unit -> small time unit: year-month-day hour:minute)
-	# Thus, you do not need this:
-	# lines = sorted(lines, key=lambda x: datetime.datetime.strptime(x[x.index("[")+1:x.index("]")], "%Y-%m-%d %H:%M").timestamp())
-	# This is enough (string sort):
-	lines.sort()
 
 	for line in lines:
 		firstpart	= line.split("] ")[0]
@@ -25,28 +23,25 @@ with open('input') as f:
 		if lastpart.startswith("Guard #"): # new guard will be processed
 			guard_id = int(lastpart.split(" ")[1][1:])
 		elif lastpart.startswith("wakes"): # last seen guard wakes up
-			guards[guard_id].update({_min : 1 for _min in range(sleepstart, minute)})
+			for m in range(sleepstart, minute):
+				sleep_per_minute[guard_id][m] += 1
 		else: # last seen guard falls asleep
 			sleepstart = minute
 
-	gid_max_sum = -1
-	max_sum = -1
-	max_minute_p1 = -1
-	gid_max_minute = -1
-	max_minute_val = -1
-	max_minute_p2 = -1
+	sum_per_guard = [(guard, sum(minutes.values())) for guard, minutes in sleep_per_minute.items()]
+	guard = int(max(sum_per_guard, key=operator.itemgetter(1))[0])
+	max_minute = max(sleep_per_minute[guard].items(), key=operator.itemgetter(1))[0]
+	print("A", guard * max_minute)
 
-	for guard_id, sleep_count in guards.items():
-		sleep_count = list(dict(sleep_count).values())
-		if sum(sleep_count) > max_sum:
-			max_sum = sum(sleep_count)
-			gid_max_sum = guard_id
-			max_minute_p1 = sleep_count.index(max(sleep_count))
+	gid = None
+	max_min_val = None
+	max_min = None
 
-		if max(sleep_count) > max_minute_val:
-			gid_max_minute = guard_id
-			max_minute_val = max(sleep_count)
-			max_minute_p2 = sleep_count.index(max_minute_val)
+	for guard, minutes in sleep_per_minute.items():
+		for minute, value in minutes.items():
+			if max_min_val is None or value > max_min_val:
+				max_min_val = value
+				gid = int(guard)
+				max_min = minute
 
-	print("A", gid_max_sum * max_minute_p1)
-	print("B", gid_max_minute * max_minute_p2)
+	print("B", gid * max_min)
