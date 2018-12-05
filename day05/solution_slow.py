2d0958943e6d2febd399a9f0db537731328fe35c
Simon Hessner <uldci@student.kit.edu>
2018-12-05 02:01:27 -0500

    Update solution for day 05

--- day05/solution_slow.py
+++ day05/solution_slow.py
@@ -1,6 +1,8 @@
 # https://adventofcode.com/2018/day/5
 # My solution runs very slow (240s), probably because of the construction of a new string everytime a reaction is found. solution_fast.py avoids this
 
+import string
+
 def findIndex(inp):
 	for i in range(len(inp)-1):
 		if ((inp[i].isupper() and inp[i+1].islower()) or (inp[i+1].isupper() and inp[i].islower())) and inp[i].lower() == inp[i+1].lower():
@@ -8,22 +10,18 @@ def findIndex(inp):
 	return None
 
 def react(s):
-	while True:
+	while True: # This is also not very optimal
 		i = findIndex(s)
 		if i is None:
 			return s
 		else:
-			s = "".join(s[idx] for idx in range(len(s)) if idx not in [i, i+1])
+			s = "".join(s[idx] for idx in range(len(s)) if idx not in [i, i+1]) # I guess this is the problematic part that slows down everything
 			
 with open('input') as f:
-	lines = f.read().splitlines()
-	inp = lines[0]
-	reacted = react(inp)
+	reacted = react(f.read().splitlines()[0])
 	print("A", len(reacted))
 	
 lengths = []
-for ch in "abcdefghijklmnopqrstuvwxyz":
-	ns = "".join(x for x in reacted if x.lower() != ch)
-	r = react(ns)
-	lengths.append(len(r))
+for ch in string.ascii_lowercase:
+	lengths.append(react("".join(x for x in reacted if x.lower() != ch)))
 print("B", min(lengths))
