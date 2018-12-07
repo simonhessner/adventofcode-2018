785befc0f224c302a6cc5db20208cc040110993b
Simon Hessner <uldci@student.kit.edu>
2018-12-07 00:57:33 -0500

    Add solution for day07 (rank 69 for task 1, rank 475 for task 2

--- day06/solution.py
+++ day06/solution.py
@@ -6,7 +6,12 @@
 # not the max in line 50 but the second max in line 51. Will try to find the
 # problem tomorrow
 
-with open('input') as f:
+from collections import defaultdict
+
+def manhattan(a, b):
+	return abs(a[0]-b[0]) + abs(a[1]-b[1])
+
+with open('testinput') as f:
 	lines = f.read().splitlines()
 	coords = []
 	xs = []
@@ -16,37 +21,33 @@ with open('input') as f:
 		xs.append(coords[-1][0])
 		ys.append(coords[-1][1])
 
-
-	from collections import defaultdict
-	is_closest = defaultdict(int)
-
 	minx, maxx = min(xs), max(xs)
 	miny, maxy = min(ys), max(ys)
-
-
-	def manhattan(a, b):
-		return abs(a[0]-b[0]) + abs(a[1]-b[1])
 
 	def find_closest(x,y):
 		points_with_dist = defaultdict(list)
 
-		for c in coords:
-			d = manhattan((x,y), c)
-			points_with_dist[d].append(c)
+		for c in coords:						
+				d = manhattan((x,y), c)
+				points_with_dist[d].append(c)
 
 		md = min(dict(points_with_dist).keys())
 		if len(points_with_dist[md]) > 1:
 			return None
 		return points_with_dist[md][0]
 
-	def infinite(cl):
+	def is_on_border(cl):
 		return cl[0] in [minx, maxx] or cl[1] in [miny, maxy]
+
+	is_closest = defaultdict(int)
 
 	for y in range(miny, maxy):
 		for x in range(minx, maxx):
 			cl = find_closest(x,y)
-			if cl is not None and not infinite(cl):
+			if cl is not None:# and not is_on_border(cl):
 				is_closest[cl] += 1
+
+	is_closest = {k :v for k,v in is_closest.items() if not is_on_border(k) }
 
 	print("A", max(dict(is_closest).values()))
 	print(sorted(dict(is_closest).values()))
