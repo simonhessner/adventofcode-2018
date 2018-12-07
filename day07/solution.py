eb5c92eb52b32984e8b6a81affe06ea38e710613
Simon Hessner <uldci@student.kit.edu>
2018-12-07 01:24:06 -0500

    Cleanup code for day 7

--- day07/solution.py
+++ day07/solution.py
@@ -1,52 +1,46 @@
 from collections import defaultdict
 
 def run(workers):
-	missing_jobs = set()
-	delay = 60
+	requirements = defaultdict(set)
+	missing_steps = set()
+	finish_order = []
 
-	with open('input') as f:
-		lines = f.read().splitlines()
-		requirements = defaultdict(set)
-		for line in lines:
+	waittimes = [0] * workers
+	works_on = [None] * workers
+
+	with open('input') as f:		
+		for line in f.read().splitlines():
 			parts = line.split(" ")
 			requirements[parts[-3]].add(parts[1])
-			missing_jobs.add(parts[1])
-			missing_jobs.add(parts[-3])
-
-		waittimes = [0] * workers
-		step_agent = [None] * workers
+			missing_steps |= set([parts[1], parts[-3]])		
 		
-		a = []
-		duration = 0
-		while len(missing_jobs) > 0:
-			for i in range(workers):
-				if waittimes[i] == 0:
-					job_finished = step_agent[i]
-					step_agent[i] = None
-					if job_finished is not None:
-						a.append(job_finished)
-						missing_jobs = missing_jobs - set([job_finished])
-						requirements = {s : r-set([job_finished]) for s,r in requirements.items()}
+	duration = 0
+	while len(missing_steps) > 0:
+		for i in range(workers):
+			if waittimes[i] == 0:
+				job_finished = works_on[i]
+				if job_finished is not None:
+					finish_order.append(job_finished)
+					missing_steps = missing_steps - set([job_finished])
+					requirements = {step : reqs-set([job_finished]) for step,reqs in requirements.items()}
+				works_on[i] = None
 
-					possible = [st for st in missing_jobs if len(requirements[st]) == 0 and st not in step_agent]
-					possible.sort()
+				possible = sorted([step for step in missing_steps if len(requirements[step]) == 0 and step not in works_on])
 
-					if len(possible) > 0:
-						nextstep = possible[0]				
-						
-						waittimes[i] = delay + 1 + ord(nextstep) - ord('A')
-						step_agent[i] = nextstep
-
-			waittimes = [max(0, x-1) for x in waittimes]
+				if len(possible) > 0:
+					works_on[i] = possible[0]					
+					waittimes[i] = 60 + ord(works_on[i]) - ord('A') + 1
 					
-			if len(missing_jobs) > 0:
-				duration += 1
 
+		waittimes = [max(0, x-1) for x in waittimes]
+				
+		if len(missing_steps) > 0:
+			duration += 1
 
-		return (duration, "".join(a))			
+	return (duration, "".join(finish_order))			
 
-result1 = run(1)
-print("Part 1\t", result1[1])
+_, job_order = run(1)
+print("Part 1\t", job_order)
 
-result2 = run(5)
-print("Part 2\t", result2[0])
+duration, _ = run(5)
+print("Part 2\t", duration)
