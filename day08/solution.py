dd3071fcfb75d9a98a6a7ca90640d08ad948c922
Simon Hessner <uldci@student.kit.edu>
2018-12-08 03:38:57 -0500

    Add alternative solution for day 08 using generators

--- day08/solution.py
+++ day08/solution.py
@@ -1,33 +1,35 @@
-from collections import defaultdict
+# https://adventofcode.com/2018/day/8
 
-def parse(unprocessed_data):
-	n_childs, n_meta = unprocessed_data[:2]
-	unprocessed_data = unprocessed_data[2:]
+# This was the hardest problem so far in AOC2018.
+
+def parse_subtree(unexplored):
+	n_childs, n_meta = unexplored[:2]
+	unexplored = unexplored[2:]
 
 	metas = []
 	child_values = []
 
 	for c in range(n_childs):
-		child_metas, unprocessed_data, child_value = parse(unprocessed_data)
+		child_metas, unexplored, child_value = parse_subtree(unexplored)
 		metas += child_metas
 		child_values.append(child_value)
 	
-	local_metas = unprocessed_data[:n_meta]
+	local_metas = unexplored[:n_meta]
 
-	metas += unprocessed_data[:n_meta]
+	metas += unexplored[:n_meta]
 
 	if n_childs == 0:
-		return metas, unprocessed_data[n_meta:], sum(local_metas)
+		return metas, unexplored[n_meta:], sum(local_metas)
 
 	value = 0
 	for ref in local_metas:
 		print(local_metas)
 		if ref in range(1, len(child_values)+1):
 			value += child_values[ref-1]
-	return metas, unprocessed_data[n_meta:], value
+	return metas, unexplored[n_meta:], value
 
 
 with open('input') as f:
-	unprocessed_data = [int(x) for x in f.read().splitlines()[0].split()]
-	metas, _, value = parse(unprocessed_data)
+	unexplored = [int(x) for x in f.read().splitlines()[0].split()]
+	metas, _, value = parse_subtree(unexplored)
 	print(sum(metas), value)
---
+++ day08/solution_with_generators.py
@@ -0,0 +1,33 @@
+# Inspired by: https://www.reddit.com/r/adventofcode/comments/a47ubw/2018_day_8_solutions/ebc82oj/
+
+"""
+The idea is to go through the input int by int. Each integer must only be read one time, so the runtime is O(n).
+
+parse_tree() takes the input stream and reads the first two integers from it. These describe the amount of child nodes and meta values.
+For each child parse_tree is called recursively, thus the stream is read further. When there is no child left, the meta values of the deepest 
+node are read and processed as required by the problem description. Then the recursion goes one level up and either continues with the next child
+or with the meta data of the parent node, and so on.
+"""
+
+def parse_tree(inp):
+	n_childs, n_meta = next(inp), next(inp)
+	metas = []
+
+	child_values = []
+	for _ in range(n_childs):
+		child_metas, child_value = parse_tree(inp)
+		metas += child_metas
+		child_values.append(child_value)
+	
+	local_metas = [next(inp) for _ in range(n_meta)]
+	metas += local_metas
+
+	return (metas, 
+		    sum(local_metas) if n_childs == 0 
+		    else (sum(child_values[ref-1] for ref in local_metas if ref-1 in range(n_childs)))
+		   )
+
+it = iter([int(x) for x in open('input').read().split(" ")])
+metas, value = parse_tree(it)
+print("Part 1", sum(metas))
+print("Part 2", value)
