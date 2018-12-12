ffd1937aa6a4a36cae017792f1901e095a841eb9
Simon Hessner <uldci@student.kit.edu>
2018-12-12 03:03:36 -0500

    Remove unneeded code

--- day12/solution.py
+++ day12/solution.py
@@ -1,9 +1,3 @@
-def find_match(rule, pots):
-	for i in range(len(pots)-5):
-		if pots[i:i+5] == rule:
-			return i+2
-	return None
-
 from collections import defaultdict
 
 def getsum(pots, first):
@@ -13,26 +7,25 @@ def getsum(pots, first):
 			s += i - first
 	return s
 
-with open('input') as f:
-	lines = f.read().splitlines()
-	pots = lines[0].split(": ")[1]
-	pots = "."*20000 + pots + "."*20000
-	first = 0
-	for i,c in enumerate(pots):
-		if c == "#":
-			first = i
-			break
-		
-	rules = defaultdict(lambda : ".")
-	for rule in lines[2:]:
-		condition, replacement = rule.split(" => ")
-		rules[condition] = replacement
+lines = open('input').read().splitlines()
+pots = lines[0].split(": ")[1]
+pots = "."*2000 + pots + "."*2000
+first = 0
+for i,c in enumerate(pots):
+	if c == "#":
+		first = i
+		break
+	
+rules = defaultdict(lambda : ".")
+for rule in lines[2:]:
+	condition, replacement = rule.split(" => ")
+	rules[condition] = replacement
 
-	for g in range(200):
-		newpots = list(pots)
-		for i in range(2, len(pots)-2):
-			newpots[i] = rules[pots[i-2:i+3]]
-		pots = "".join(newpots)
-		s = getsum(pots, first)
-		
-	print((50000000000-200)*87+s)
+for g in range(200):
+	newpots = list(pots)
+	for i in range(2, len(pots)-2):
+		newpots[i] = rules[pots[i-2:i+3]]
+	pots = "".join(newpots)
+	s = getsum(pots, first)
+	
+print((50000000000-200)*87+s)
